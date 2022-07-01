ed116fef198448c5fc58bf6b84d52b87ab9e08f5
Ahmed Abdelfattah Ahmed <ahmed.a.elfattah.ahmed@gmail.com>
2022-07-01 20:22:29 +0200

    add app launcher

--- app/src/main/java/com/example/myapplication/viewPatient/PdfWriterManager.java
+++ app/src/main/java/com/example/myapplication/viewPatient/PdfWriterManager.java
@@ -3,11 +3,9 @@ package com.example.myapplication.viewPatient;
 import android.content.Context;
 import android.widget.Toast;
 
-import com.example.myapplication.Session;
 import com.example.myapplication.data.model.VentilatorSession;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
-
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
@@ -15,7 +13,6 @@ import com.itextpdf.layout.property.TextAlignment;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class PdfWriterManager {
